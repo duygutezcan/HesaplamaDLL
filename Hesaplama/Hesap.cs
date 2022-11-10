4db92ab45bf5bc32b3f6b1b6f454a36764ec2af1
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-10 09:13:46 +0300

    Add project files.

---
+++ Hesaplama/Hesap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hesaplama
+{
+    public class Hesap
+    {
+        public static decimal Topla(decimal a , decimal b)
+        {
+            return a + b;
+        }
+
+        public static decimal Cikar(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        public static decimal Bol(decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+        public static decimal Carp(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        public static double KokAl(double a)
+        {
+            return Math.Sqrt(a);
+        }
+
+    }
+}
